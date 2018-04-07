a0e2e6b294e3c0837f06ada9a330b09b9ce9f121
cbonoz <chrisdistrict@gmail.com>
2018-04-07 00:57:07 -0700

    use library import

--- cryptogateway/src/App.js
+++ cryptogateway/src/App.js
@@ -2,8 +2,15 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import plugin from './plugin'
+
 class App extends Component {
-  render() {
+
+    componentWillMount() {
+        plugin.setup();
+    }
+
+    render() {
     return (
       <div className="App">
         <header className="App-header">
--- jsplugin/plugin.js
+++ jsplugin/plugin.js
@@ -1,5 +1,48 @@
-/**
- * Created by cbuonocore on 4/6/18.
- */
+const library = (function () {
+    // Axios required for now (could use native http module if dependencyless).
+    const axios = require('axios');
 
+    console.log('plugin');
+    function addStyleString(str) {
+        const node = document.createElement('style');
+        node.innerHTML = str;
+        document.body.appendChild(node);
+    }
+
+    const blur = `html {
+        -webkit-filter: blur(3px);
+        -moz-filter: blur(3px);
+        filter: blur(3px);
+    }`;
+
+    function showPaywallDialog() {
+        // var win = openDialog("http://example.tld/zzz.xul", "dlg", "", "pizza", 6.98);
+        // const myWindow = window.open("", "", "width=200, height=100");
+        // myWindow.document.write("<p>A new window!</p>");
+        // myWindow.blur();
+        let popupWin = window.open('', '', 'top=0,left=0,height=100%,width=auto');
+
+        axios.post('/user', {
+            firstName: 'Fred',
+            lastName: 'Flintstone'
+        })
+            .then(function (response) {
+                console.log(response);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
+
+    function setup() {
+        addStyleString(blur);
+        showPaywallDialog();
+    }
+
+    return {
+        setup: setup
+    }
+
+})();
+module.exports = library;
 
