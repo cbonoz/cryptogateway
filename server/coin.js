bc82abdf0e1f6c72dc72f6a3500eb89a39b89336
cbonoz <chrisdistrict@gmail.com>
2018-04-07 01:00:15 -0700

    reformat

--- server/coin.js
+++ server/coin.js
@@ -3,20 +3,20 @@
  */
 
 let secp256k1 = require('secp256k1')
-let { randomBytes } = require('crypto')
+let {randomBytes} = require('crypto')
 let createHash = require('sha.js')
 let vstruct = require('varstruct')
 let axios = require('axios')
 
 let TxStruct = vstruct([
-    { name: 'amount', type: vstruct.UInt64BE },
-    { name: 'senderPubKey', type: vstruct.Buffer(33) },
-    { name: 'senderAddress', type: vstruct.Buffer(32) },
-    { name: 'receiverAddress', type: vstruct.Buffer(32) },
-    { name: 'nonce', type: vstruct.UInt32BE }
+    {name: 'amount', type: vstruct.UInt64BE},
+    {name: 'senderPubKey', type: vstruct.Buffer(33)},
+    {name: 'senderAddress', type: vstruct.Buffer(32)},
+    {name: 'receiverAddress', type: vstruct.Buffer(32)},
+    {name: 'nonce', type: vstruct.UInt32BE}
 ])
 
-exports.handler = function(state, rawTx) {
+exports.handler = function (state, rawTx) {
     let tx = deserializeTx(rawTx)
     if (!verifyTx(tx)) {
         return
@@ -28,7 +28,7 @@ exports.handler = function(state, rawTx) {
     let senderBalance = state.balances[senderAddress] || 0
     let receiverBalance = state.balances[receiverAddress] || 0
 
-    if(senderAddress === receiverAddress) {
+    if (senderAddress === receiverAddress) {
         return
     }
     if (!Number.isInteger(tx.amount)) {
@@ -66,7 +66,7 @@ function hashTx(tx) {
 function signTx(privKey, tx) {
     let txHash = hashTx(tx)
     let signedTx = Object.assign({}, tx)
-    let { signature } = secp256k1.sign(txHash, privKey)
+    let {signature} = secp256k1.sign(txHash, privKey)
     signedTx.signature = signature
 
     return signedTx
@@ -113,54 +113,54 @@ function deriveAddress(pubKey) {
         .digest()
 }
 
-exports.client = function(url = 'http://localhost:3232') {
+exports.client = function (url = 'http://localhost:3232') {
     let methods = {
-            generatePrivateKey: () => {
+        generatePrivateKey: () => {
             let privKey
             do {
                 privKey = randomBytes(32)
             } while (!secp256k1.privateKeyVerify(privKey))
 
-    return privKey
-},
-    generatePublicKey: privKey => {
-        return secp256k1.publicKeyCreate(privKey)
-    },
-    generateAddress: pubKey => {
-        return deriveAddress(pubKey)
-    },
-    getBalance: async (address) => {
-        let state = await axios.get(url + '/state').then(res => res.data)
-        return state.balances[address] || 0
-    },
-    send: async (privKey, { address, amount }) => {
-        let senderPubKey = methods.generatePublicKey(privKey)
-        let senderAddress = methods.generateAddress(senderPubKey)
+            return privKey
+        },
+        generatePublicKey: privKey => {
+            return secp256k1.publicKeyCreate(privKey)
+        },
+        generateAddress: pubKey => {
+            return deriveAddress(pubKey)
+        },
+        getBalance: async (address) => {
+            let state = await axios.get(url + '/state').then(res => res.data)
+            return state.balances[address] || 0
+        },
+        send: async (privKey, {address, amount}) => {
+            let senderPubKey = methods.generatePublicKey(privKey)
+            let senderAddress = methods.generateAddress(senderPubKey)
 
-        let currentState = await axios.get(url + '/state').then(res => res.data)
+            let currentState = await axios.get(url + '/state').then(res => res.data)
 
-        let nonce = currentState.nonces[senderAddress.toString('hex')] || 0
+            let nonce = currentState.nonces[senderAddress.toString('hex')] || 0
 
-        let receiverAddress
-        if (typeof address === 'string') {
-            receiverAddress = Buffer.from(address, 'hex')
-        } else {
-            receiverAddress = address
+            let receiverAddress
+            if (typeof address === 'string') {
+                receiverAddress = Buffer.from(address, 'hex')
+            } else {
+                receiverAddress = address
+            }
+            let tx = {
+                amount,
+                senderPubKey,
+                senderAddress,
+                receiverAddress,
+                nonce
+            }
+
+            let signedTx = signTx(privKey, tx)
+            let serializedTx = serializeTx(signedTx)
+            let result = await axios.post(url + '/txs', serializedTx)
+            return result.data
         }
-        let tx = {
-            amount,
-            senderPubKey,
-            senderAddress,
-            receiverAddress,
-            nonce
-        }
-
-        let signedTx = signTx(privKey, tx)
-        let serializedTx = serializeTx(signedTx)
-        let result = await axios.post(url + '/txs', serializedTx)
-        return result.data
     }
-}
 
     return methods
 }
